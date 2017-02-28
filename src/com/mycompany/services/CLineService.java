17d40541628b8a97b5a741e10f8067764fd7539a
lzou-canada <lizou.canada@gmail.com>
2017-02-27 21:35:32 -0500

    Add one more line in cline service for the Services repo

--- src/com/mycompany/services/CLineService.java
+++ src/com/mycompany/services/CLineService.java
@@ -5,6 +5,7 @@ package com.mycompany.services;
  */
 public class CLineService extends Service
 {
+    // add one line here for the Services repo testing
     public CLineService(String name)
     {
         super(name);
