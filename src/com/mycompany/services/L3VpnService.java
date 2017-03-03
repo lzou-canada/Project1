814509a005555b4d6b5df6920483113aa1d361af
lzou-canada <lizou.canada@gmail.com>
2017-03-02 21:15:48 -0500

    Add one line here to further move repo for PTS-122

--- src/com/mycompany/services/L3VpnService.java
+++ src/com/mycompany/services/L3VpnService.java
@@ -13,6 +13,7 @@ public class L3VpnService extends Service
     @Override
     protected ServiceType getServiceType()
     {
+        // add one move line here to further move PTS-122 branch
         // add one line her to move the PTS-122 branch
         // add another line here to further move it
         // add something here for L3VPN for Services for PTS-122
