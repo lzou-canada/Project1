9bd43f169adf29b3a4da1d5c2ed29fa0b8a89dc6
lzou-canada <lizou.canada@gmail.com>
2017-03-02 22:24:25 -0500

    Simplified comments in L3VPN to move the master branch

--- src/com/mycompany/services/L3VpnService.java
+++ src/com/mycompany/services/L3VpnService.java
@@ -13,15 +13,7 @@ public class L3VpnService extends Service
     @Override
     protected ServiceType getServiceType()
     {
-        // add one move line here to further move PTS-122 branch
-        // add one line her to move the PTS-122 branch
-        // add one line to further move the master branch
-        // add one line here to move the master branch
-        // add one move line here to further move PTS-122 branch
-        // add one line her to move the PTS-122 branch
-        // add another line here to further move it
-        // add something here for L3VPN for Services for PTS-122
-        // add one line for feature-2
+        // simplified the comments
         return ServiceType.SERVICE_TYPE_L3VPN;
     }
 }
