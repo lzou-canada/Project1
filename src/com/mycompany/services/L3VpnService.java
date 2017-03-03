c1859319ce80d856c15451fd7137412c97d93f25
lzou-canada <lizou.canada@gmail.com>
2017-03-02 21:22:40 -0500

    Add one line here to further move the master branch

--- src/com/mycompany/services/L3VpnService.java
+++ src/com/mycompany/services/L3VpnService.java
@@ -13,6 +13,7 @@ public class L3VpnService extends Service
     @Override
     protected ServiceType getServiceType()
     {
+        // add one line to further move the master branch
         // add one line here to move the master branch
         // add another line here to further move it
         // add something here for L3VPN for Services for PTS-122
